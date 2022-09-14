e03e37cd76110d70ef2e2149275f05fb25394de6
Philippe Lacasse <philac105@hotmail.com>
2022-09-14 13:39:28 -0400

    removed sequelize fr

--- server/database/connection.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { host: process.env.DB_ADDRESS, dialect: "mysql" });
-
-module.exports = sequelize;
-global.sequelize = sequelize;
