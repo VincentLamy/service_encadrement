3bfdcda7bfee68975d0c4415e0a940b3a83f1466
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2022-09-22 19:02:53 -0400

    Create semesters/comments panel

--- client/src/views/StudentFormView.vue
+++ client/src/views/StudentFormView.vue
@@ -1,9 +1,12 @@
 <template>
   <v-container>
-    
     <h1 class="my-2">Nom de l'étudiant</h1>
 
-    <!-- Informations de l'étudiants -->
+    <!--
+      ************************************************
+      Informations de l'étudiant
+      ************************************************ 
+    -->
     <v-card class="py-2 px-3 mb-5">
       <v-card-text>
         <h2 class="d-flex justify-center">Informations de l'étudiant</h2>
@@ -39,7 +42,11 @@
       </v-card-actions>
     </v-card>
 
-    <!-- Informations de l'étudiant à l'international -->
+    <!--
+      ************************************************
+      Informations de l'étudiant à l'international
+      ************************************************ 
+    -->
     <v-card class="py-2 px-3 mb-5">
       <v-card-text>
         <h2 class="d-flex justify-center">
@@ -63,7 +70,11 @@
       </v-card-actions>
     </v-card>
 
-    <!-- Questionnaire de mathématiques de l'étudiant (Sec.4 & Sec.5) -->
+    <!--
+      ************************************************
+      Questionnaire de mathématiques
+      ************************************************ 
+    -->
     <v-card class="py-2 px-3 mb-5">
       <v-card-text>
         <h2 class="d-flex justify-center">Questionnaire de mathématiques</h2>
@@ -143,13 +154,75 @@
       </v-card-actions>
     </v-card>
 
-    <!-- Cours de l'étudiant & Commentaires des professeurs -->
-    <v-card class="py-2 px-3 mb-5">
+    <!--
+      ************************************************
+      Cours & Commentaires 
+      ************************************************ 
+    -->
+
+    <v-card>
       <v-card-text>
         <h2 class="d-flex justify-center">Commentaires</h2>
+
+        <!-- Onglets sessions -->
+        <v-tabs v-model="semesters.tab">
+          <v-tab v-for="code in semesters.codes">
+            {{ code }}
+          </v-tab>
+        </v-tabs>
+
+        <!-- Cours & Commentaires des sessions -->
+        <v-tabs-items v-model="semesters.tab">
+          <v-tab-item v-for="(code, i) in semesters.codes" :key="i">
+            <v-btn color="primary my-3"> Ajouter un commentaire </v-btn>
+
+            <!-- Cours de la session -->
+            <v-expansion-panels accordion>
+              <v-expansion-panel v-for="j in 5" :key="j">
+                <v-expansion-panel-header>
+                  Cours #{{ j }}
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <!-- Commentaires d'un cours -->
+                  <v-list>
+                    <v-list-item v-for="k in 3" :key="k">
+                      <v-list-item-content>
+                        <v-list-item-title
+                          >Commentaire #{{ k }}</v-list-item-title
+                        >
+                        <v-list-item-subtitle
+                          >Informations supplémentaires</v-list-item-subtitle
+                        >
+                      </v-list-item-content>
+                      <v-list-item-action>
+                        <v-icon>mdi-pencil-outline</v-icon>
+                      </v-list-item-action>
+                    </v-list-item>
+                  </v-list>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+
+            <!-- Commentaires de la session -->
+            <v-list>
+              <v-list-item v-for="k in 3" :key="k">
+                <v-list-item-content>
+                  <v-list-item-title
+                    >Commentaire sur la session #{{ k }}</v-list-item-title
+                  >
+                  <v-list-item-subtitle
+                    >Informations additionnelles</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+                <v-list-item-action>
+                  <v-icon>mdi-pencil-outline</v-icon>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list>
+          </v-tab-item>
+        </v-tabs-items>
       </v-card-text>
     </v-card>
-
   </v-container>
 </template>
 
@@ -161,8 +234,12 @@ export default {
       math_form: {
         start_time: null,
         end_time: null,
-      }
-    }
-  }
+      },
+      semesters: {
+        tab: null,
+        codes: ["AUT21", "HIV21", "AUT22", "HIV22"],
+      },
+    };
+  },
 };
 </script>
