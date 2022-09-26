efb7b5b7cf230e9cb0772f0c6eab825cbd11b3af
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2022-09-26 15:17:43 -0400

    Add responsive & margins

--- client/src/views/StudentFormView.vue
+++ client/src/views/StudentFormView.vue
@@ -10,9 +10,9 @@
     <v-card class="py-2 px-3 mb-5">
       <v-card-text>
         <h2 class="d-flex justify-center">Informations de l'étudiant</h2>
-        <v-row>
+        <v-row no-gutters>
           <!-- Numéro de dossier -->
-          <v-col>
+          <v-col class="px-3" lg="3" sm="6" cols="12">
             <v-text-field
               label="No. de dossier"
               type="number"
@@ -21,22 +21,22 @@
           </v-col>
 
           <!-- Code permanent -->
-          <v-col>
+          <v-col class="px-3" lg="3" sm="6" cols="12">
             <v-text-field label="Code permanent" />
           </v-col>
 
           <!-- Service d'enseignement -->
-          <v-col>
+          <v-col class="px-3" lg="3" sm="6" cols="12">
             <v-combobox label="Service d'enseignement" />
           </v-col>
 
           <!-- Numéro de programme -->
-          <v-col>
+          <v-col class="px-3" lg="3" sm="6" cols="12">
             <v-select label="No. de programme" />
           </v-col>
         </v-row>
       </v-card-text>
-      <v-card-actions class="d-flex justify-space-between">
+      <v-card-actions class="px-5 d-flex justify-space-between">
         <v-btn color="grey" disabled plain>Annuler</v-btn>
         <v-btn type="submit" color="primary" plain>Mettre à jour</v-btn>
       </v-card-actions>
@@ -52,19 +52,19 @@
         <h2 class="d-flex justify-center">
           Informations de l'étudiant à l'international
         </h2>
-        <v-row>
+        <v-row no-gutters>
           <!-- Statut -->
-          <v-col>
+          <v-col class="px-3" sm="6" cols="12">
             <v-select label="Statut" />
           </v-col>
 
           <!-- Pays d'origine -->
-          <v-col>
+          <v-col class="px-3" sm="6" cols="12">
             <v-select label="Pays d'origine" />
           </v-col>
         </v-row>
       </v-card-text>
-      <v-card-actions class="d-flex justify-space-between">
+      <v-card-actions class="px-5 d-flex justify-space-between">
         <v-btn color="grey" disabled plain>Annuler</v-btn>
         <v-btn type="submit" color="primary" plain>Mettre à jour</v-btn>
       </v-card-actions>
@@ -78,26 +78,29 @@
     <v-card class="py-2 px-3 mb-5">
       <v-card-text>
         <h2 class="d-flex justify-center">Questionnaire de mathématiques</h2>
-        <v-row>
-
-          <!-- Heure de début & fin -->
-          <v-col>
+        <v-row no-gutters>
+          <!-- Heure de début -->
+          <v-col class="px-3" lg="3" sm="6" cols="12">
             <v-datetime-picker label="Heure de début" />
+          </v-col>
+
+          <!-- Heure de fin -->
+          <v-col class="px-3" lg="3" sm="6" cols="12">
             <v-datetime-picker label="Heure de fin" />
           </v-col>
 
           <!-- Effort fourni -->
-          <v-col>
+          <v-col class="px-3" lg="3" sm="6" cols="12">
             <v-textarea label="Effort fourni" />
           </v-col>
 
           <!-- Expérience en informatique -->
-          <v-col>
+          <v-col class="px-3" lg="3" sm="6" cols="12">
             <v-textarea label="Expérience en informatique" />
           </v-col>
         </v-row>
       </v-card-text>
-      <v-card-actions class="d-flex justify-space-between">
+      <v-card-actions class="px-5 d-flex justify-space-between">
         <v-btn color="grey" disabled plain>Annuler</v-btn>
         <v-btn type="submit" color="primary" plain>Mettre à jour</v-btn>
       </v-card-actions>
@@ -109,7 +112,7 @@
       ************************************************ 
     -->
 
-    <v-card>
+    <v-card class="py-2 px-3 mb-5">
       <v-card-text>
         <h2 class="d-flex justify-center">Commentaires</h2>
 
@@ -120,56 +123,70 @@
           </v-tab>
         </v-tabs>
 
-        <!-- Cours & Commentaires des sessions -->
-        <v-tabs-items v-model="semesters.tab">
-          <v-tab-item v-for="(code, i) in semesters.codes" :key="i">
-            <v-btn color="primary my-3"> Ajouter un commentaire </v-btn>
+        <v-container>
+          <!-- Cours & Commentaires des sessions -->
+          <v-tabs-items v-model="semesters.tab">
+            <v-tab-item v-for="(code, i) in semesters.codes" :key="i">
+              <v-btn color="primary my-3"> Ajouter un commentaire </v-btn>
 
-            <!-- Cours de la session -->
-            <v-expansion-panels accordion>
-              <v-expansion-panel v-for="j in 5" :key="j">
-                <v-expansion-panel-header>
-                  Cours #{{ j }}
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  <!-- Commentaires d'un cours -->
-                  <v-list>
-                    <v-list-item v-for="k in 3" :key="k">
-                      <v-list-item-content>
-                        <v-list-item-title
-                          >Commentaire #{{ k }}</v-list-item-title
-                        >
-                        <v-list-item-subtitle
-                          >Informations supplémentaires</v-list-item-subtitle
-                        >
-                      </v-list-item-content>
-                      <v-list-item-action>
-                        <v-icon>mdi-pencil-outline</v-icon>
-                      </v-list-item-action>
-                    </v-list-item>
-                  </v-list>
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-            </v-expansion-panels>
+              <!-- Cours de la session -->
+              <v-expansion-panels accordion>
+                <v-expansion-panel v-for="j in 5" :key="j">
+                  <v-expansion-panel-header>
+                    Cours #{{ j }} {{ code }}
+                  </v-expansion-panel-header>
+                  <v-expansion-panel-content>
+                    <!-- Commentaires d'un cours -->
+                    <v-list>
+                      <v-list-item v-for="k in 3" :key="k">
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            Commentaire #{{ k }}
+                          </v-list-item-title>
 
-            <!-- Commentaires de la session -->
-            <v-list>
-              <v-list-item v-for="k in 3" :key="k">
-                <v-list-item-content>
-                  <v-list-item-title
-                    >Commentaire sur la session #{{ k }}</v-list-item-title
-                  >
-                  <v-list-item-subtitle
-                    >Informations additionnelles</v-list-item-subtitle
-                  >
-                </v-list-item-content>
-                <v-list-item-action>
-                  <v-icon>mdi-pencil-outline</v-icon>
-                </v-list-item-action>
-              </v-list-item>
-            </v-list>
-          </v-tab-item>
-        </v-tabs-items>
+                          <v-list-item-subtitle>
+                            Informations supplémentaires
+                          </v-list-item-subtitle>
+                        </v-list-item-content>
+                        <v-list-item-action>
+                          <v-icon>mdi-pencil-outline</v-icon>
+                        </v-list-item-action>
+                      </v-list-item>
+                    </v-list>
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+              </v-expansion-panels>
+
+              <!-- Commentaires de la session -->
+              <v-list>
+                <template v-for="k in 3">
+                  <v-list-item>
+                    <v-list-item-content :key="k">
+                      <v-list-item-title
+                        >Commentaire sur la session #{{ k }}</v-list-item-title
+                      >
+                      <v-list-item-subtitle
+                        >Informations additionnelles</v-list-item-subtitle
+                      >
+                    </v-list-item-content>
+                    <v-list-item-action>
+                      <div class="d-flex justify-end">
+                        <v-chip class="ms-1" x-small>Remarque</v-chip>
+                        <v-chip class="ms-1" x-small>AUTN</v-chip>
+                      </div>
+                      <p>Lapalme, Jocelyn 24 sept. 2021 10:15</p>
+                      <!--<v-icon>mdi-pencil-outline</v-icon>-->
+                    </v-list-item-action>
+                  </v-list-item>
+                  <v-divider
+                    v-if="k < 3"
+                    :key="k"
+                  ></v-divider>
+                </template>
+              </v-list>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-container>
       </v-card-text>
     </v-card>
   </v-container>
