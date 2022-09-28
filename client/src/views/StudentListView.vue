dfa04f6aebf0dc3df21e70ff56d4d544c64b22a0
Vincent <202045777@cegepsherbrooke.qc.ca>
2022-09-28 10:35:26 -0400

    Liste étudiante (0.1.0)

--- client/src/views/StudentListView.vue
+++ client/src/views/StudentListView.vue
@@ -3,19 +3,31 @@
       <h2>Liste des étudiants</h2>
 
       <v-data-table
+      id="list_student"
       :headers="headers"
       :items="students"
       :sort-desc="[false, true]"
-      multi-sort
       class="elevation-1"
-      @click:row="rowClick"
       >
-        <template v-slot:header.student_critical_State>
+        <template v-slot:header.student_critical_state>
           <v-icon color="red">mdi-alert-circle</v-icon>
         </template>
 
-        <template v-slot:item.student_critical_state>
-          <v-icon color="red">mdi-alert-circle</v-icon>
+        <template v-slot:body="{ items }">
+          <tr v-for="item in items" :key="item.student_number" v-on:click="rowClick(item.student_number)">
+            <td v-if="item.critical_course_quantity > 3">
+              <v-icon color="red">mdi-alert-circle</v-icon>
+              {{ change_critical_state_value(item) }}
+            </td>
+            <td v-else>
+              <v-icon></v-icon>
+            </td>
+            <td>{{ item.student_number }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.statut }}</td>
+            <td>{{ item.commentary_quantity }}</td>
+            <td>{{ item.critical_course_quantity }}</td>
+          </tr>
         </template>
     </v-data-table>
   </v-container>
@@ -26,7 +38,7 @@
       data () {
         return {
           headers: [
-            { text: '', value: 'student_critical_State'},
+            { text: '', value: 'student_critical_state'},
             { text: 'Numéro étudiant', value: 'student_number' },
             { text: 'Nom complet', value: 'name' },
             { text: 'Statut étudiant', value: 'statut' },
@@ -40,7 +52,7 @@
               name: 'Vincent Lamy',
               statut: 'Services adaptés',
               commentary_quantity: '2',
-              critical_course_quantity: '0',
+              critical_course_quantity: '3',
             },
             { 
               student_critical_state: '',
@@ -48,18 +60,26 @@
               name: 'Vincent Deux',
               statut: 'Sous Contrat',
               commentary_quantity: '2',
-              critical_course_quantity: '0',
+              critical_course_quantity: '12',
             }
           ]
         }
     },
     methods: {
-        rowClick(item, row) {
-            let URL = `/student_form/${item.student_number}`
-            window.location = URL;
-            window.location.href = URL;
-            window.location.assign(URL);
+      rowClick(student_number) {
+          let URL = `/student_form/${student_number}`
+          window.location = URL;
+          window.location.href = URL;
+          window.location.assign(URL);
+      },
+      change_critical_state_value(item) {
+        if (item.student_critical_state == true) {
+          item.student_critical_state == false;
         }
-    }
-}
+        else if (item.student_critical_state == false) {
+          item.student_critical_state = true;
+        }
+      }
+    } 
+  }
   </script>
