f360e4325e4af0572a1d80b054f6ed919b64ab77
Philippe Lacasse <philac105@hotmail.com>
2022-10-08 20:32:02 -0400

    fixed migrations fr

--- server/prisma/migrations/20221009001429_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Groupe_code_session_fkey` ON `Groupe`;
-
--- AddForeignKey
-ALTER TABLE `Groupe` ADD CONSTRAINT `Groupe_code_session_fkey` FOREIGN KEY (`code_session`) REFERENCES `Session`(`code`) ON DELETE RESTRICT ON UPDATE CASCADE;
