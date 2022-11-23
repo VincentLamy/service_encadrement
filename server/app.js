62b15d9edb97397ab3ad456a1d9af3e5534e449f
Philippe Lacasse <philac105@hotmail.com>
2022-11-23 11:07:26 -0500

    l

--- server/app.js
+++ server/app.js
@@ -12,7 +12,6 @@ const port = process.env.PORT || 5000;
 // routes prefix
 app.use('/api', require('./routes/routes'));
 
-
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/dist'));
     app.get('*', (req, res) => {
